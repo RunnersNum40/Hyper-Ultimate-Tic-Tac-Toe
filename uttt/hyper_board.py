a614cc9a861a20bf73a095a4f226ec94ace73eef
Ted Pinkerton <ted.pinkerton@mail.utoronto.ca>
2022-12-02 19:07:04 -0500

    Added a marker class

--- uttt/hyper_board.py
+++ uttt/hyper_board.py
@@ -3,16 +3,6 @@ from itertools import product
 
 import numpy as np
 
-
-def ternary_list(n):
-    if n == 0:
-        return [0]
-    nums = []
-    while n:
-        n, r = divmod(n, 3)
-        nums.append(r)
-    return nums
-
 def direction_vectors(n: int):
     """Generate all posible direction vectors in n-dimensional space with unit length basis vectors.
 
@@ -23,11 +13,40 @@ def direction_vectors(n: int):
         All direction vectors in n-dimensional space with unit components
     """
     # Yield all combinations of the unit vectors
-    zero = tuple(0 for _ in range(n))
-    return np.array([direction for direction in product([-1, 0, 1], repeat=n) if direction != zero], dtype=np.int32)
+    directions = np.array([direction for direction in product([1, 0, -1], repeat=n)], dtype=np.int32)
+    for i in range(directions.shape[0], 0, -1):
+        if (-directions[i-1]).tolist() in directions.tolist():
+            directions = np.delete(directions, (i-1), axis=0)
+    return directions
+
+def check_move(board, position):
+    """Check if a point on a board is in a winning line"""
+    pass
 
 
-class HyperBoard:
+class BoardMarker:
+    """A marker placed on a board"""
+    def __init__(self):
+        raise NotImplementedError
+
+    def value(self):
+        """Value used to score games"""
+        raise NotImplementedError
+
+    def __str__(self):
+        return f"Player: {self.value()}"
+
+
+class Player(BoardMarker):
+    """A marker that contains the name of the player that places it"""
+    def __init__(self, name) -> None:
+        self.name = name
+
+    def value(self):
+        return self.name
+
+
+class HyperBoard(BoardMarker):
     """An n-dimensional hypercubic tic-tac-toe board
 
     Params:
@@ -35,7 +54,7 @@ class HyperBoard:
         winner: integer marker of the winner if one exists
         shape: shape of the board (n length tuple of equal integer side lengths)
     """
-    def __init__(self, side_length: int = 3, dimensions: int = 2, players: int = 2) -> None:
+    def __init__(self, side_length: int = 3, dimensions: int = 2) -> None:
         """A local board composed of a hypercube of positions.
 
         Args:
@@ -45,31 +64,34 @@ class HyperBoard:
         """
         self.winner = None
         self.shape = tuple(side_length for _ in range(dimensions))
-        self.board = np.full(self.shape, np.nan)
+        self.board = np.empty(self.shape, dtype=object)
 
         self.ndims = dimensions
         self.side_length = side_length
 
-        self.directions = direction_vectors(self.ndims)
+        self._directions = direction_vectors(self.ndims)
 
-    def place_marker(self, player: int, position: tuple) -> bool:
+    def place_marker(self, marker, position: tuple) -> bool:
         """Place a marker on the board and return if it wins
 
         Args:
-            player: Integer marker of the player
+            player: Board Marker object of the player
             position: n-dimensional position in the board
         Returns:
             True if the move wins the board, False if the move does not win the board.
         """
+        # Allow integer players
+        if type(marker) is int:
+            marker = Player(marker)
         # Check that the move is legal
-        if not np.isnan(self.board[position]):
+        if self.board[position] is not None:
             raise MoveError(f"{self.board[position]} already in position")
         # Place the marker
-        self.board[position] = player
+        self.board[position] = marker
         # Check if the move wins
         position = np.array(position, dtype=np.int32)
         # Vary the point placed in each possible direction
-        for direction in self.directions:
+        for direction in self._directions:
             # Find the range possible in the given direction
             test_min = -self.ndims
             test_max = self.ndims+1
@@ -84,16 +106,96 @@ class HyperBoard:
             if test_max-test_min == self.side_length:
                 # Find all points along the line
                 line = [self.board[tuple(position+i*direction)] for i in range(test_min, test_max)]
+                line = [marker.value() if marker is not None else None for marker in line]
                 # Check if all markers are the same
-                if len(set(line)) == 1:
+                if None not in line and len(set(line)) == 1:
                     # The placement wins the board
-                    self.winner = player
+                    self.winner = marker
                     return True
         # Not a winning placement
         return False
+
+    def value(self):
+        return self.winner
 
     def __str__(self):
         if self.board.ndim <= 3:
             return str(self.board)
         else:
             return f"{self.board.ndim}-dimensional board with side length of {self.shape[0]}"
+
+
+class StandardBoard(HyperBoard):
+    def __init__(self):
+        super().__init__(side_length = 3, dimensions = 2)
+
+
+if __name__ == '__main__':
+    marker = Player(1)
+    player = StandardBoard()
+    player.place_marker(marker, (0, 0))
+    player.place_marker(marker, (1, 0))
+    player.place_marker(marker, (2, 0))
+
+    # Test if a board allows placing on a spot twice
+    board = HyperBoard(1, 1)
+    assert board.place_marker(marker, (0, ))
+    try:
+        board.place_marker(marker, (0, ))
+    except MoveError:
+        pass
+
+    # Test a normal board for row, column, and diagonal wins
+    # Row
+    board = HyperBoard(3, 2)
+    assert not board.place_marker(player, (0, 1))
+    assert not board.place_marker(player, (1, 1))
+    assert     board.place_marker(player, (2, 1))
+    # Column
+    board = HyperBoard(3, 2)
+    assert not board.place_marker(player, (1, 0))
+    assert not board.place_marker(player, (1, 1))
+    assert     board.place_marker(player, (1, 2))
+    # Diagonal
+    board = HyperBoard(3, 2)
+    assert not board.place_marker(player, (0, 0))
+    assert not board.place_marker(player, (1, 1))
+    assert     board.place_marker(player, (2, 2))
+    # Anti-diagonal
+    board = HyperBoard(3, 2)
+    assert not board.place_marker(player, (2, 0))
+    assert not board.place_marker(player, (1, 1))
+    assert     board.place_marker(player, (0, 2))
+    # Test a 3D board
+    board = HyperBoard(3, 3)
+    assert not board.place_marker(player, (0, 1, 1))
+    assert not board.place_marker(player, (1, 1, 1))
+    assert     board.place_marker(player, (2, 1, 1))
+    board = HyperBoard(3, 3)
+    assert not board.place_marker(player, (1, 0, 1))
+    assert not board.place_marker(player, (1, 1, 1))
+    assert     board.place_marker(player, (1, 2, 1))
+    board = HyperBoard(3, 3)
+    assert not board.place_marker(player, (1, 1, 0))
+    assert not board.place_marker(player, (1, 1, 1))
+    assert     board.place_marker(player, (1, 1, 2))
+    board = HyperBoard(3, 3)
+    assert not board.place_marker(player, (2, 0, 0))
+    assert not board.place_marker(player, (1, 1, 0))
+    assert     board.place_marker(player, (0, 2, 0))
+    board = HyperBoard(3, 3)
+    assert not board.place_marker(player, (0, 2, 0))
+    assert not board.place_marker(player, (0, 1, 1))
+    assert     board.place_marker(player, (0, 0, 2))
+    board = HyperBoard(3, 3)
+    assert not board.place_marker(player, (0, 0, 2))
+    assert not board.place_marker(player, (1, 0, 1))
+    assert     board.place_marker(player, (2, 0, 0))
+    board = HyperBoard(3, 3)
+    assert not board.place_marker(player, (0, 0, 0))
+    assert not board.place_marker(player, (1, 1, 1))
+    assert     board.place_marker(player, (2, 2, 2))
+    board = HyperBoard(3, 3)
+    assert not board.place_marker(player, (2, 2, 2))
+    assert not board.place_marker(player, (1, 1, 1))
+    assert     board.place_marker(player, (0, 0, 0))
